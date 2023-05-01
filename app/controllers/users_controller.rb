8db311c53cbdf9e9203a8828c6d2e92ca7fe62e8
Hashem Sowande Gray <hgray1109@gmail.com>
2023-05-01 18:00:05 -0400

    Delete users, trackable data, error in schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,9 @@ class UsersController < ApplicationController
     end
       
       
-
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path, notice: "User was successfully destroyed."
+    end
 end
