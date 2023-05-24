85e9248d172966d2f4b76049c5694b42db4e962f
Hashem Sowande Gray <hgray1109@gmail.com>
2023-05-23 20:46:50 -0400

    calander search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:landing_page, :privacy_policy]
   
   def landing_page
   end
@@ -7,7 +8,20 @@ class StaticPagesController < ApplicationController
   end
 
   def calendar
-    @lessons = Lesson.all
+    if params.has_key?(:user_id)
+      lessons = Lesson.includes(:user, :classroom, :course, :attendances)
+      if params[:user_id].present?
+        lessons = lessons.where(user_id: params[:user_id])
+      end
+      if params[:classroom_id].present?
+        lessons = lessons.where(classroom_id: params[:classroom_id])
+      end
+      if params[:course_id].present?
+        lessons = lessons.where(course_id: params[:course_id])
+      end
+      @lessons = lessons.all
+    else
+      @lessons = Lesson.includes(:user, :classroom, :course, :attendances)
+    end
   end
-
 end
