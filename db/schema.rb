e6ff5e1819dd1948303fe874d60954b55d43b180
Hashem Sowande Gray <hgray1109@gmail.com>
2023-05-25 13:06:03 -0400

    student_price_final column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_25_004509) do
+ActiveRecord::Schema.define(version: 2023_05_25_162748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_05_25_004509) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "student_price_start", default: 0, null: false
+    t.integer "student_price_final", default: 0, null: false
     t.index ["lesson_id"], name: "index_attendances_on_lesson_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
